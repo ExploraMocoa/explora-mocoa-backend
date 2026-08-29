99de401f51b153cd2faaef3e64fc940808ca2a27
Ana Sofia Hoyos <ana.sha18@outlook.com>
2026-08-29 00:12:46 -0500

    chore: inicializa estructura base de Spring Boot

---
+++ src/main/java/com/example/exploramocoa/ExploramocoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.exploramocoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploramocoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploramocoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exploramocoa/ExploramocoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exploramocoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploramocoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
